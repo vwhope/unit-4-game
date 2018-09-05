5c0d8817df880fab53c9fb0be69684090df8ae4a
Grover <41809783+vwhope@users.noreply.github.com>
2018-09-05 15:45:55 -0400

    add html, css layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,30 @@
 
 // ================================ BEGIN GLOBAL VARIABLE DEFINITIONS =======================================================
 // set GLOBAL variables - available to all functions - (generally don't want global- is better to make an encapsulated object
-
-
-
-
+var totWins = 0;
+var totLoss = 0;
+var yourScore = 0;
+var randomNumber = 0;
+var jewel1Val = 0;
+var jewel2Val = 0;
+var jewel3Val = 0;
+var jewel4Val = 0;
 
 // ================================ END GLOBAL VARIABLE DEFINITIONS =========================================================
 
 // ================================ BEGIN FUNCTION DEFINITIONS  =============================================================
-
-
+function generateRandomNbr() {
+        
+    var min = 19;
+    var max = 120;
+    
+    randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(randomNumber);
+  
+    // insert random number into our random-number div.
+    $("#randomNumber").text(randomNumber);
+    
+} // end generateRandomNbr
 
 
 
@@ -21,6 +35,7 @@
 // HTML page loads FIRST, then this code runs
 // If any event listeners/handlers have been registerd (ex. document.onkeyup) they are still listening
 
+generateRandomNbr();
 
 
 
